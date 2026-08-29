7222bfda90a257f80c00267749af65e97e13aa44
soenneker <jake@soenneker.com>
2026-08-29 14:17:07 -0500

    Improve public API XML documentation

--- src/Soenneker.HealthSherpa.HttpClients/Abstract/IHealthSherpaOpenApiHttpClient.cs
+++ src/Soenneker.HealthSherpa.HttpClients/Abstract/IHealthSherpaOpenApiHttpClient.cs
@@ -2,7 +2,6 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading;
-
 namespace Soenneker.HealthSherpa.HttpClients.Abstract;
 
 /// <summary>
@@ -10,5 +9,10 @@ namespace Soenneker.HealthSherpa.HttpClients.Abstract;
 /// </summary>
 public interface IHealthSherpaOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured HTTP client used by the Health Sherpa OpenAPI HTTP Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested HTTP client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.HealthSherpa.HttpClients/Registrars/HealthSherpaOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.HealthSherpa.HttpClients/Registrars/HealthSherpaOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class HealthSherpaOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="HealthSherpaOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddHealthSherpaOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class HealthSherpaOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="HealthSherpaOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddHealthSherpaOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
